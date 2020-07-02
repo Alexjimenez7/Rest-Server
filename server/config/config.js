29d4840a031e98c395b42052af040846a051041d
Alex <alex.j2k@gmail.com>
2020-07-02 09:45:48 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://iron_man:vEhMdk3Oou4eSe8Z@cluster0.3rldm.mongodb.net/cafe'
+    //urlDB = 'mongodb+srv://iron_man:vEhMdk3Oou4eSe8Z@cluster0.3rldm.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
